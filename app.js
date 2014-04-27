4d16b2f05d33c6baab84ca83ee0546b936aac4ad
Scott Smith <scottksmith95@gmail.com>
2014-04-27 10:23:55 -0700

    Added modules for export call

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 var request = require('request');
 
-exports = function (shortUrl, callback) {
+module.exports = function (shortUrl, callback) {
   request( { method: "HEAD", url: shortUrl, followAllRedirects: true },
     function (error, response) {
       if (error) { 
