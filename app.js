145fa3000a1beede7b516cd1478df4c32c430944
Scott Smith <scottksmith95@gmail.com>
2014-06-19 08:36:07 -0700

    If HEAD fails try GET

--- app.js
+++ app.js
@@ -1,12 +1,17 @@
 var request = require('request');
 
 module.exports = function (shortUrl, callback) {
-  request( { method: "HEAD", url: shortUrl, followAllRedirects: true },
-    function (error, response) {
-      if (error) { 
-        callback(error, null);
-      } else {
-        callback(null, response.request.href);
-      }
+  request({ method: "HEAD", url: shortUrl, followAllRedirects: true }, function (error, response) {
+    if (error) {
+      request({ method: "GET", url: shortUrl, followAllRedirects: true }, function (error, response) {
+        if (error) {
+          callback(error, null);
+        } else {
+          callback(null, response.request.href);
+        }
+      });
+    } else {
+      callback(null, response.request.href);
+    }
   });
 };
