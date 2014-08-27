58c37383240412b9338cea4640de4b723e8aef87
Scott Smith <scottksmith95@gmail.com>
2014-08-27 15:39:24 -0700

    Added cookie support for sites that need it

--- app.js
+++ app.js
@@ -1,15 +1,23 @@
 var request = require('request');
+var tough = require('tough-cookie');
 
 module.exports = function (shortUrl, callback) {
-  request({ method: "HEAD", url: shortUrl, followAllRedirects: true }, function (error, response) {
+  var useragent = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/36.0.1985.143 Safari/537.36';
+  var jar = request.jar();
+    
+  var options = {
+    method: 'GET',
+    url: shortUrl,
+    followAllRedirects: true,
+    headers: { 
+      'User-Agent': useragent
+    },
+    jar: jar
+  };
+
+  request(options, function (error, response) {
     if (error) {
-      request({ method: "GET", url: shortUrl, followAllRedirects: true }, function (error, response) {
-        if (error) {
-          callback(error, null);
-        } else {
-          callback(null, response.request.href);
-        }
-      });
+      callback(error, null);
     } else {
       callback(null, response.request.href);
     }
